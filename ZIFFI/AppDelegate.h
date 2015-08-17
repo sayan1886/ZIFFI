ec775a1704d8b0a38c9f2b25e30fd74b3e14a994
SayanC <sayan1886@gmail.com>
2015-08-17 22:27:47 +0530

    Initial Commit

---
+++ ZIFFI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZIFFI
+//
+//  Created by Sayan Chatterjee on 17/08/15.
+//  Copyright (c) 2015 ZIFFI.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
